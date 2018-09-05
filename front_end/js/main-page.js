1b674ef1186425d2b84870bfe9a45982348211f1
mambalex <alexzhangzhiqin@gmail.com>
2018-09-05 16:29:37 +1000

    updated tags

--- front_end/js/main-page.js
+++ front_end/js/main-page.js
@@ -11,3 +11,26 @@ $(".minus").click(function(){
 	$(this).hide()
 	$(".plus").show()
 })
+
+$(document).on('click', '.plus-tag', function(e){
+	var flg = 1;
+	e.preventDefault();
+	var new_tag = $(".tag-input").val();
+	if(new_tag.match(/^ *$/) === null){
+		$(".tag-labels li").each(function(){
+			alert($(this).find(".tg").text());
+			if(new_tag === $(this).find(".tg").text() && $(this).css('display') !== "none" ){
+				flg = 0;
+			}else if(new_tag === $(this).find(".tg").text() && $(this).css('display') === "none"){
+				flg = 0;
+				$(this).css('display',"inline-block");
+			}
+		});
+	if(flg === 1){$(".tag-labels").append("<li><span class='tg'>" + new_tag + "</span> <span class='x'>x</span></li>");}
+}})
+
+
+$(document).on('click', '.tag-labels li span', function(e){
+	e.preventDefault();
+	$(this).parent().fadeOut("slow");
+})
